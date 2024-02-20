9802c03f0bc97f6061d00e5a683a3e6eb8205c2d
evidad <errolvidad@gmail.com>
2024-02-20 14:04:20 -0800

    added permissions file with read only access or edit if owner access

--- doggos/views.py
+++ doggos/views.py
@@ -1,11 +1,14 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Doggo
 from .serializers import DoggoSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class DoggoListView(ListCreateAPIView):
     queryset = Doggo.objects.all()
     serializer_class = DoggoSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
 class DoggoDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Doggo.objects.all()
     serializer_class = DoggoSerializer
+    permission_classes = [IsOwnerOrReadOnly]
